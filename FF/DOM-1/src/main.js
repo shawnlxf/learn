0c1c3f6c670ab248420305d6c916ff69f51e4f79
shawn <1838435618@qq.com>
2020-08-05 23:44:37 +0800

    update

--- FF/DOM-1/src/main.js
+++ FF/DOM-1/src/main.js
@@ -15,7 +15,7 @@ const title = dom.attr(test, 'title')
 console.log(`title: ${title}`)
 
 dom.text(test, '你好，这是新的内容')
-dom.text(test)
+console.log(dom.text(test))
 
 dom.style(test, { border: '1px solid red', color: 'blue' })
 console.log(dom.style(test, 'border'))
@@ -30,7 +30,7 @@ const fn = () => {
     console.log('点击了')
 }
 dom.on(test, 'click', fn)
-dom.off(test, 'click', fn)
+// dom.off(test, 'click', fn)
 
 const testDiv = dom.find('#test')[0]
 console.log(testDiv)
---
+++ FF/DOM-2/src/main.js
@@ -0,0 +1,23 @@
+// jQuery 是全局变量
+/*
+const api = jQuery('.test')
+api.addClass('red').addClass('blue') // 链式操作
+*/
+
+jQuery('.test')
+    .find('.child')
+    .addClass('red')
+    .addClass('blue')
+    .addClass('green')
+    .end()
+    .addClass('yellow')
+
+const arr1 = jQuery('.test')
+    .find('.child')
+
+arr1.each(value => console.log(value))
+
+const arr2 = jQuery('.test')
+arr2.parent().print()
+
+arr2.children().print()
