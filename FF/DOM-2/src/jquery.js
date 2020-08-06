65bd88b01fcd35611632d14ff77640237d31ca83
shawn <1838435618@qq.com>
2020-08-06 19:55:31 +0800

    update

--- FF/DOM-2/src/jquery.js
+++ FF/DOM-2/src/jquery.js
@@ -1,66 +1,114 @@
-window.jQuery = function (selectorOrArray) {
+window.$ = window.jQuery = function (selectorOrArrayOrTemplate) {
     let elements
-    if (typeof selectorOrArray === 'string') {
-        elements = document.querySelectorAll(selectorOrArray)
-    } else if (selectorOrArray instanceof Array) {
-        elements = selectorOrArray
-    }
-    // api 可以操作 elements
-    return {
-        // 闭包：函数访问外部的变量
-        addClass(className) {
-            /*
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(className)
-            }
-            */
-            this.each(n =>{
-                n.classList.add(className)
-            })
-            return this
-        },
-        oldApi: selectorOrArray.oldApi,
-        find(selector) {
-            let array = []
-            /*
-            for (let i = 0; i < elements.length; i++) {
-                const newElements = Array.from(elements[i].querySelectorAll(selector))
-                array = array.concat(newElements)
-            }
-            */
-            this.each(n => {
-                array.push(...n.querySelectorAll(selector))
-            })
-            array.oldApi = this
-            return jQuery(array)
-        },
-        end() {
-            return this.oldApi
-        },
-        each(fn) {
-            for (let i = 0; i < elements.length; i++) {
-                fn.call(null, elements[i], i)
-            }
-            return this
-        },
-        parent() {
-            const array = []
-            this.each(node => {
-                if (array.indexOf(node.parentNode) === -1) {
-                    array.push(node.parentNode)
-                }
-            })
-            return jQuery(array)
-        },
-        children() {
-            const array = []
-            this.each(node => {
-                array.push(...node.children)
-            })
-            return jQuery(array)
-        },
-        print() {
-            console.log(elements)
+    if (typeof selectorOrArrayOrTemplate === 'string') {
+        if (selectorOrArrayOrTemplate[0] === '<') {
+            // 创建 div
+            elements = [createElement(selectorOrArrayOrTemplate)]
+        } else {
+            // 查找 div
+            elements = document.querySelectorAll(selectorOrArrayOrTemplate)
         }
+    } else if (selectorOrArrayOrTemplate instanceof Array) {
+        elements = selectorOrArrayOrTemplate
+    }
+
+    function createElement(string) {
+        const container = document.createElement("template")
+        container.innerHTML = string.trim()
+        return container.content.firstChild
+    }
+
+    // api 可以操作 elements
+    // 创建一个对象，这个对象的 __proto__ 为括号里的东西
+    const api = Object.create(jQuery.prototype)
+    //api.elements = elements
+    //api.oldApi = selectorOrArrayOrTemplate.oldApi
+    Object.assign(api, {
+        elements: elements,
+        oldApi: selectorOrArrayOrTemplate.oldApi
+    })
+
+    return api
+}
+
+jQuery.fn = jQuery.prototype = {
+    constructor: jQuery,
+    jquery: true,
+    get(index) {
+        return this.elements[index]
+    },
+    appendTo(node) {
+        if (node instanceof Element) {
+            this.each(el => node.appendChild(el))
+        } else if (node.jquery === true) {
+            this.each(el => node.get(0).appendChild(el))
+        }
+
+    },
+    append(children) {
+        if (children instanceof Element) {
+            this.get(0).appendChild(children)
+        } else if (children instanceof HTMLCollection) {
+            for (let i = 0; i < children.length; i++) {
+                this.get(0).appendChild(children[i])
+            }
+        } else if (children.jquery === true) {
+            children.each(node => this.get(0).appendChild(node))
+        }
+
+    },
+    // 闭包：函数访问外部的变量
+    addClass(className) {
+        /*
+        for (let i = 0; i < this.elements.length; i++) {
+            this.elements[i].classList.add(className)
+        }
+        */
+        this.each(n => {
+            n.classList.add(className)
+        })
+        return this
+    },
+    find(selector) {
+        let array = []
+        /*
+        for (let i = 0; i < this.elements.length; i++) {
+            const newthis.elements = Array.from(this.elements[i].querySelectorAll(selector))
+            array = array.concat(newthis.elements)
+        }
+        */
+        this.each(n => {
+            array.push(...n.querySelectorAll(selector))
+        })
+        array.oldApi = this
+        return jQuery(array)
+    },
+    end() {
+        return this.oldApi
+    },
+    each(fn) {
+        for (let i = 0; i < this.elements.length; i++) {
+            fn.call(null, this.elements[i], i)
+        }
+        return this
+    },
+    parent() {
+        const array = []
+        this.each(node => {
+            if (array.indexOf(node.parentNode) === -1) {
+                array.push(node.parentNode)
+            }
+        })
+        return jQuery(array)
+    },
+    children() {
+        const array = []
+        this.each(node => {
+            array.push(...node.children)
+        })
+        return jQuery(array)
+    },
+    print() {
+        console.log(this.elements)
     }
 }
--- FF/DOM-2/src/main.js
+++ FF/DOM-2/src/main.js
@@ -4,6 +4,7 @@ const api = jQuery('.test')
 api.addClass('red').addClass('blue') // 链式操作
 */
 
+/*
 jQuery('.test')
     .find('.child')
     .addClass('red')
@@ -21,3 +22,10 @@ const arr2 = jQuery('.test')
 arr2.parent().print()
 
 arr2.children().print()
+*/
+
+const $div = $('<div><span>1</span></div>')
+const $childList = $('.child')
+$('body').append($childList)
+
+//$('<div>1</div>').appendTo(document.body)
